a5b4e1f80780e58b91eec7041776a704fd720887
lucasaug <luc.aug.freire@gmail.com>
2014-05-04 00:27:28 -0300

    Linhas e retângulos agora aceitam que a segunda coordenada seja menor que a primeira

--- photocommand.c
+++ photocommand.c
@@ -20,18 +20,37 @@ void colorir(char bitmap[MAX_LIN][MAX_COL], int x, int y, char cor){
 
 void colorir_linha_vertical(char bitmap[MAX_LIN][MAX_COL], int x, int y1, int y2, char c){
 	int j;
+	// corrigindo linhas com coordenadas invertidas (y2 < y1)
+	if(y1 > y2){
+		int aux = y2;
+		y2 = y1;
+		y1 = aux;
+	}
 	for(j = y1; j <= y2; j++)
 		colorir(bitmap, x, j, c);
 }
 
 void colorir_linha_horizontal(char bitmap[MAX_LIN][MAX_COL], int x1, int x2, int y, char c){
 	int i;
+	// corrigindo linhas com coordenadas invertidas (x2 < x1)
+	if(x1 > x2){
+		int aux = x2;
+		x2 = x1;
+		x1 = aux;
+	}
 	for(i = x1; i <= x2; i++)
 		colorir(bitmap, i, y, c);
 }
 
 void desenhar_retangulo(char bitmap[MAX_LIN][MAX_COL], int x1, int x2, int y1, int y2, char c){
 	int i;
+	// corrigindo retangulos com coordenadas invertidas (y2 < y1)
+	// x2 < x1 é corrigido na funcao colorir_linha_horizontal
+	if(y1 > y2){
+		int aux = y2;
+		y2 = y1;
+		y1 = aux;
+	}
 	for(i = y1; i <= y2; i++)
 		colorir_linha_horizontal(bitmap, x1,x2, i, c);
 }
